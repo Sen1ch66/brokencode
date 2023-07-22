d38c7146e77cc0c773d9dd31e4b66a448cda6360
Sen1ch66 <129590795+Sen1ch66@users.noreply.github.com>
2023-07-22 12:40:38 +0300

    Ти можеш почінити його

---
+++ AnimatingThroughJS_Spoiler/js/app.js
@@ -0,0 +1,14 @@
+$('.spoiler').hide();
+
+$('.spoilerButton').on('click', function () {
+  $('.spoilerButton').hide();
+  $('.spoiler').slideDown(1000);
+});
+$('.spoiler2').hide();
+
+$('spoilerButton2').on('click', function () {
+  $('spoilerButton2').hide();
+  $('spoiler2').slideDown(1000);
+});
+
+
